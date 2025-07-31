b1ff6bc0c61e539260fd7c036549bd0829e6f237
Lee <hsophally.student@puthisastra.edu.kh>
2025-07-31 20:27:08 +0700

    fixed bugs in admin dashboard

--- resources/views/frontend/scripts.blade.php
+++ resources/views/frontend/scripts.blade.php
@@ -1,16 +1,16 @@
 <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="js/jquery-3.2.1.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/stellar.js"></script>
-  <script src="vendors/lightbox/simpleLightbox.min.js"></script>
-  <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
-  <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
-  <script src="vendors/isotope/isotope-min.js"></script>
-  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
-  <script src="js/jquery.ajaxchimp.min.js"></script>
-  <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
-  <script src="vendors/counter-up/jquery.counterup.js"></script>
-  <script src="js/mail-script.js"></script>
-  <script src="js/theme.js"></script>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{ asset('frontend/assets/js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/popper.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/stellar.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/lightbox/simpleLightbox.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/nice-select/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/isotope/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/isotope/isotope-min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/counter-up/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/counter-up/jquery.counterup.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/mail-script.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/theme.js') }}"></script>
